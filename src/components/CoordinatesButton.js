94cd985568653a3969aaca59929e667982cfb303
Noah Scala <nwscala@gmail.com>
2020-04-24 10:22:57 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,17 @@
 // Code CoordinatesButton Component Here
+import React, { Component } from 'react';
+
+export default class CoordinatesButton extends Component {
+    findCoordinates = (event) => {
+        const coordinateArray = [
+            event.clientX,
+            event.clientY
+        ]
+
+        this.props.onReceiveCoordinates(coordinateArray)
+    }
+    
+    render() {
+        return <button onClick={this.findCoordinates}></button>
+    }
+}
